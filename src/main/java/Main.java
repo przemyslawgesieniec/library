11f619d8a611176d11db798c6bd98019499c8405
Przemyslaw Gesieniec <x_Gesiepr1@corp.onespan.com>
2020-04-25 10:29:22 +0200

    intial UI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Robimy biblioteke");
+        System.out.println("Witaj w bibliotece! Co mogę dla ciebie zrobić?");
+        while (true) {
+            menu();
+            int option = SCANNER.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("wyswietln Not implemented yet");
+                    break;
+                case 2:
+                    System.out.println("Dodaj Not implemented yet");
+                    break;
+                case 3:
+                    System.out.println("Usun Not implemented yet");
+                    break;
+                default:
+                    System.out.println("Operacja o wybranym numerze nie istnieje");
+                    break;
+            }
+        }
+    }
+
+    private static void menu() {
+        System.out.println("1) Wyświetl wszystkie książki");
+        System.out.println("2) Dodaj książkę");
+        System.out.println("3) Usuń książkę");
+        System.out.println("Wybieram: ");
     }
 }
